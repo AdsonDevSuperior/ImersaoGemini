b2773bdb192965882574e70ecd4210be6caa7fef
Silva, Adson Albino Manoel Da <adson.albino.m.silva@accenture.com>
2024-09-09 20:25:50 -0300

    Imersao Gemini initial

---
+++ app.js
@@ -0,0 +1,53 @@
+function pesquisar() {
+    // documento forma usada para pegar os dados do html
+    let section = document.getElementById("resultados-pesquisa")
+    console.log(section);
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    const regex = /^[a-zA-Z]{4,}$/;
+
+    if (!campoPesquisa) {
+        section.innerHTML = "Nada foi encontrado"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    if (!regex.test(campoPesquisa)) {
+        section.innerHTML = "Digite apenas letras e mais de 3 caracteres";
+        return;
+    }
+    console.log(campoPesquisa)
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+        <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="https://ge.globo.com/olimpiadas/guia/2024/07/13/c-rebeca-andrade-idade-altura-medalhas
+                -e-historia-da-ginasta.ghtml" target="_blank">Mais
+                    informações</a>
+            </div>
+`;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado. Você precisa digitar o nome do atreta ou esporte</p>"
+    }
+    section.innerHTML = resultados
+}
+
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Nascida em Imperatriz, Maranhão, ela se tornou a mais jovem medalhista olímpica da história do Brasil, ao conquistar o bronze no skate street nos Jogos Olímpicos de Tóquio 2020.",
+        link: "https://www.olimpiadatododia.com.br/rayssa-leal/",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Judoca brasileira, campeã olímpica nos Jogos de Paris 2024. Conhecida por sua força e técnica, Beatriz conquistou a primeira medalha de ouro do Brasil no judô na história dos Jogos Olímpicos.",
+        link: "https://cbj.com.br/atletas/beatriz-souza/",
+        tags: "judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Ginasta brasileira, campeã olímpica e mundial. Conquistou a primeira medalha de ouro olímpica para o Brasil na ginástica artística, nos Jogos de Tóquio 2020.",
+        link: "https://ge.globo.com/olimpiadas/guia/2024/07/13/c-rebeca-andrade-idade-altura-medalhas-e-historia-da-ginasta.ghtml",
+        tags: "ginástica ginastica ouro"
+    }
+];
